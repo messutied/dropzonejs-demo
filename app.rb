f9ab13c5941087e2e67f89b5c7530d50c8db7d27
Eduardo Messuti <messuti.edd@gmail.com>
2013-12-04 14:56:43 -0400

    no js version

--- app.rb
+++ app.rb
@@ -8,5 +8,5 @@ post '/file_upload' do
   File.open('uploads/' + params['file'][:filename], "w") do |f|
     f.write(params['file'][:tempfile].read)
   end
-  return "The file was successfully uploaded!"
+  redirect to "/?uploaded=true"
 end
